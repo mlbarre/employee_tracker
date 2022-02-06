9f48e60c127b901aa94586306452b37c4b8937a2
mlb <mlbarrera1538@gmail.com>
2022-02-06 15:48:30 -0600

    added beginning functions, need to polish code and finalize fs

--- server.js
+++ server.js
@@ -1,7 +1,21 @@
 const inquirer = require('inquirer');
 const sequelize = require('sequelize');
-
+const cTable = require('console.table');
 const fs = require('fs');
+
+const db = mysql.createConnection({
+    host: "localhost",
+    PORT: 3001,
+    user: "root",
+    password: "0921%mlb",
+    database: "office_db",
+});
+db.connect((err) => {
+    if (err) {
+        throw err;
+    }
+    console.log("Successfully connected!");
+});
 
 const questions = inquirer
     .prompt([
@@ -9,53 +23,73 @@ const questions = inquirer
             type: 'list',
             name: 'choices',
             message: 'What would you like to do?',
-            choices: ['Add Department', 'Add Role', 'Add Employee', 'Update Employee']
-        },
-        { 
+            choices: ['Add Department', 'Add Role', 'Add Employee', 'Update Employee'],
+            validate(answer) {
+                if (!answer) {
+                console.log("Please enter a valid choice.");
+            }},
+        }]).then((answers) => {
         // department input
-        when: input => {
-            (input.choices === 'Add Department');
-        },
-            type: 'input',
-            name: 'newDept',
-            message: "What is the name of the new Department?"
-        },
-        {
+        if (choices === 'Add Department') {
+            addNewDepartment();
+        }
         // role input 
-            when: input => {
-                (input.choices === 'Add Role');
-            },
-            type: 'input',
-            name: 'newRole',
-            message: 'What is the name of this new Role?'
-        },
+        if (choices === 'Add Role') {
+            addNewRole();
+        } 
+        // employee input
+        if (choices === 'Add Employee') {
+            addNewEmployee();
+        }
+        if (choices === 'Update Employee') {
+            updateEmployeeData();
+        };
+
+const addNewEmployee = () => {
+    prompt([
         {
-            type: 'input',
-            name: 'roleName',
-            message: 'What is the name of this role?'
-        },
-        {
-            type: 'input',
-            name: 'payRate',
-            message: 'What is the salary of this role?'
-        },
-        {
-            when: input => {
-                (input.choices === 'Add Employee');
-            },
             type: 'input',
             name: 'firstName',
-            message: "What is this employee's first name?"
+            message: "What is this employee's first name?",
+            validate(answer) {
+                if (!answer) {
+                    console.log("Please enter a valid name.");
+                }
+            }
         },
         {
             type: 'input',
             name: 'lastName',
-            message: "What is this employee's last name?"
+            message: "What is this employee's last name?",
+            validate(answer) {
+                if (!answer) {
+                    console.log("Please enter a valid name.");
+                }
+            }
         },
         {
             type: 'input',
             name: 'role',
-            message: "What is this employee's role?"
-        }
-
-    ])
+            message: "What is this employee's role?",
+            validate(answer) {
+                if (!answer) {
+                    console.log("Please enter a valid name.");
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'payRate',
+            message: 'What is the salary of this employee?',
+        },
+        ]).then((answer) => {
+            let data = [answer.firstName, answer.lastName, answer.payRate];
+            const syncSQL = `INSERT INTO employee (first_name, last_name, role_id)
+                        VALUES (?,?,?)`;
+            connection.query(syncSQL, data, (err) => {
+                if (err) throw err;
+                console.log('Something went wrong!');
+            },
+            console.log('Employe was added into the database!');
+            }),
+        });
